30caa3030e4a84acebee6af0856120b39db48a09
阳之幸福 <1689484241@qq.com>
2022-04-06 19:33:38 +0800

    Wed Apr  6 19:33:21 CST 2022 Ready First tdd

---
+++ chapter03/003.tdd/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestCase1(t *testing.T) {
+	inputRecord("王强",0.38)
+	inputRecord("王强",0.32)
+	{
+		randOfWQ, fatRateOfWQ := getRank("王强")
+		if randOfWQ != 1 {
+			t.Fatalf("预期 王强 第一，但是得到的是： %d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.32 {
+			t.Fatalf("预期王强的体脂是0.32, 但得到的是: %f", fatRateOfWQ)
+		}
+	}
+	inputRecord("李静",0.28)
+	randOfLJ, fatRateOfLJ := getRank("李静")
+	{
+		if randOfLJ != 1 {
+			t.Fatalf("预期 李静 第一，但是得到的是： %d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期王强的体脂是0.28, 但得到的是: %f", fatRateOfLJ)
+		}
+	}
+}
+func TestCase2(t *testing.T) {
+
+}
+
+
