2398da7c99ea47c40b03556153ec572857b30dde
阳之幸福 <1689484241@qq.com>
2022-04-10 20:52:24 +0800

    Sun Apr 10 20:52:13 CST 2022 type foce

--- chapter04/06-specialtypes/type1.go
+++ chapter04/06-specialtypes/type1.go
@@ -8,79 +8,102 @@ import (
 type Math = int
 type English = int
 type Chinese = int
+
 func main() {
 	var mathScore int = 100
 	var mathScore2 Math = 100
-	fmt.Println(mathScore,mathScore2)
+	fmt.Println(mathScore, mathScore2)
 	mathScore2 = mathScore
 	fmt.Println(mathScore2)
 	fmt.Println(getScoreOfStudent(""))
 	vg := &voteGame{
 		students: []*student{
 			&student{
-				name: fmt.Sprintf("%d",1),
+				name: fmt.Sprintf("%d", 1),
 			},
 			&student{
-				name: fmt.Sprintf("%d",2),
+				name: fmt.Sprintf("%d", 2),
 			},
 			&student{
-				name: fmt.Sprintf("%d",3),
+				name: fmt.Sprintf("%d", 3),
 			},
 			&student{
-				name: fmt.Sprintf("%d",4),
+				name: fmt.Sprintf("%d", 4),
 			},
 			&student{
-				name: fmt.Sprintf("%d",5),
-			},
-			&student{
-				name: fmt.Sprintf("%d",6),
-			},
-			&student{
-				name: fmt.Sprintf("%d",7),
+				name: fmt.Sprintf("%d", 5),
 			},
 		},
 	}
 	leader := vg.goRun()
 	fmt.Println(leader)
+	leader.Distribute()
+
+	var stdTF = &student{
+		name: "TF",
+	}
+	var ldTF Leader = Leader(*stdTF)
+	ldTF.Distribute()
+
+	bytesTest1 := []byte{}
+	var str1 string = string(bytesTest1)
+	fmt.Println(str1)
 }
 
-func getScoreOfStudent(name string) (Math,Chinese,English) {
-	return 0,0,0
+func getScoreOfStudent(name string) (Math, Chinese, English) {
+	return 0, 0, 0
 }
-
 
 type voteGame struct {
 	students []*student
 }
 
-func (g *voteGame) goRun() *leader {
+// 嵌套对象定义（继承）方式来定义班长
+//type Leader struct {
+//	student
+//}
+func (l *Leader) Distribute() {
+	fmt.Println("send homework")
+}
+
+// 使用类型重定义
+type Leader student
+
+type FooTestFuncRedefine func()
+type FooTestFuncRedefine1 []string // 类型可以是任意的
+
+func (f *FooTestFuncRedefine) test111() {
+
+}
+
+func (g *voteGame) goRun() *Leader {
 	// todo ....
-	for _,item := range g.students{
-		rnadInt := rand.Int()
-		if rnadInt %2 == 0 {
-			item.voteA(g.students[rnadInt%len(g.students)])
+	for _, item := range g.students {
+		randInt := rand.Int()
+		if randInt%2 == 0 {
+			item.voteA(g.students[randInt%len(g.students)])
 		} else {
-			item.voteA(g.students[rnadInt%len(g.students)])  // todo 用随机数代替
+			item.voteA(g.students[randInt%len(g.students)]) // todo 用随机数代替
 		}
 	}
 	maxScore := -1
 	maxScoreIndex := -1
-	for i,item := range  g.students {
+	for i, item := range g.students {
 		if maxScore < item.agree {
 			maxScore = item.agree
 			maxScoreIndex = i
 		}
 	}
-	if maxScoreIndex >= 0 {  // 判断是否大于0，因为没有学生，那么index就是默认值-1
-		return g.students[maxScoreIndex]
+	if maxScoreIndex >= 0 { // 判断是否大于0，因为没有学生，那么index就是默认值-1
+		return (*Leader)(g.students[maxScoreIndex]) // 强制转换
 	}
 	return nil
 }
 
-type leader = student
+//type Leader = student
 type student struct {
-	name string
-	agree int
+	name     string
+	agree    int
 	disagree int
 }
 
