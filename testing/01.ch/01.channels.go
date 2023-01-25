ad099276104b8dd7c97cfef1d2f81446536ff39d
阳之幸福 <1689484241@qq.com>
2023-01-25 19:46:47 +0800

    add select case

--- testing/01.ch/01.channels.go
+++ testing/01.ch/01.channels.go
@@ -78,3 +78,39 @@ func main4() {
 		fmt.Println(x)
 	}
 }
+
+func main5() {
+	var c chan struct{} // nil
+	select {
+	case <-c:
+	case c <- struct{}{}:
+	default:
+		fmt.Println("GO Here")
+	}
+}
+
+func main() {
+	c := make(chan string, 2)
+	trySend := func(v string) {
+		select {
+		case c <- v:
+		default: // 如果C的缓冲已满，则执行默认分支
+
+		}
+	}
+	tryReceive := func() string {
+		select {
+		case v := <-c:
+			return v
+		default:
+			return "-" // 如果C的缓冲为空，则执行默认分支
+		}
+	}
+	trySend("Hello")          // 发送成功
+	trySend("Hi")             // 发送成功
+	trySend("Bye")            // 发送失败，但不会阻塞
+	fmt.Println(tryReceive()) // Hello
+	fmt.Println(tryReceive()) //Hi
+	// 下面这个行将接收失败
+	fmt.Println(tryReceive()) // -
+}
